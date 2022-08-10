74a37156985afd5844669d5ceb91e28aec5b6f59
Ali Matter <109444224+AHM1998@users.noreply.github.com>
2022-08-10 15:18:55 +0200

    Add files via upload

---
+++ Showing Modal/script.js
@@ -0,0 +1,31 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  //   console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    openModal();
+  });
+
+  btnCloseModal.addEventListener('click', function () {
+    closeModal();
+  });
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
+}
